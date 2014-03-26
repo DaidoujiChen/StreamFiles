d6aabad217b81f85d9cd42f302c7e7e09cfcb58c
DaidoujiChen <daidoujichen@gmail.com>
2014-03-26 09:48:59 +0800

    Initial Commit

---
+++ StreamFiles/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StreamFiles
+//
+//  Created by 啟倫 陳 on 2014/3/26.
+//  Copyright (c) 2014年 ChilunChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
